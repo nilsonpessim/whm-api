8618e3d93e0d8ede773eee4d57431e1a3c87f741
Nilson Pessim <nilson@techlabs.net.br>
2022-07-06 13:49:51 -0300

    Update WHM.php

--- source/App/WHM.php
+++ source/App/WHM.php
@@ -5,17 +5,39 @@ namespace Source\App;
 use PreviewTechs\cPanelWHM\WHM\Accounts;
 use PreviewTechs\cPanelWHM\WHMClient;
 
+/**
+ * Classe de Conexão ao WHM
+ * Nilson Pessim
+ * 06/07/2022
+ */
 class WHM
 {
+    /**
+     * @var
+     */
     private $client;
+    /**
+     * @var
+     */
     private $account;
 
+    /**
+     * @param $username
+     * @param $token
+     * @param $url
+     * @param $port
+     * @return WHMClient
+     */
     public function client($username = CONF_WHM_USER, $token = CONF_WHM_TOKEN, $url = CONF_WHM_URL, $port = CONF_WHM_PORT): WHMClient
     {
         $this->client = new WHMClient($username, $token, $url, $port);
         return $this->client;
     }
 
+    /**
+     * @param $client
+     * @return Accounts
+     */
     public function account($client): Accounts
     {
         $this->account = new Accounts($client);
